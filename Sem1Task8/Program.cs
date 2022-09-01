45989c57dac75d0264475f0e7c8adce8fc7960ee
Yuliya <yulyakam85@gmail.com>
2022-09-01 10:44:02 +0400

    Solving .\Sem1Task8

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿//==================================================================
+// Программа, которая принимает на вход числo N, а на выходе показывает
+// все четные числа от 1 до N
+//==================================================================
+Console.Write("Введите число больше единицы ");
+string? Number = Console.ReadLine();
+if ( Number != null )
+{
+int NumberN = int.Parse(Number);
+int count = 1;
+while (count < NumberN)
+{  
+        if (count % 2 == 0) 
+        {
+            Console.Write(count);
+            Console.Write(","); 
+        }  
+    count++;
+}
+}
