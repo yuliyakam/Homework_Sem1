757ca63fdfa3eb79719f5c000c9dd5f4b60c173f
Yuliya <yulyakam85@gmail.com>
2022-09-01 10:51:25 +0400

    Add comment

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -3,27 +3,27 @@
 // какое число большее, а какое меньшее
 //=================================================================
 Console.Write("Введите первое число ");
-string? firstNumber = Console.ReadLine();
+string? firstNumber = Console.ReadLine();//Не пустая строка считывается из консоли
 Console.Write("Введите второе число ");
 string? secondNumber = Console.ReadLine();
-
-    if (firstNumber != null&&secondNumber != null)
-{
+    if (firstNumber != null&&secondNumber != null)// Проверяем, что значения не нулевые, для фунции
+    // Parse (преобразования строки в число)
+    {
     int first = int.Parse(firstNumber);
     int second = int.Parse(secondNumber);
-if ( first > second )
-{
-    Console.Write("Большее число: ");
-    Console.WriteLine(first);
-    Console.Write("Меньшее число: ");
-    Console.WriteLine(second);
-}   
-else
-{
-    Console.Write("Большее число: ");
-    Console.WriteLine(second);
-    Console.Write("Меньшее число: ");
-    Console.WriteLine(first);
-}
+        if ( first > second )
+        {
+            Console.Write("Большее число: ");
+            Console.WriteLine(first);
+            Console.Write("Меньшее число: ");
+            Console.WriteLine(second);
+        }   
+        else
+        {
+            Console.Write("Большее число: ");
+            Console.WriteLine(second);
+            Console.Write("Меньшее число: ");
+            Console.WriteLine(first);
+        }
 }
 
