b9d848a8d8ac775b07764018fff0189a7195318a
Yuliya <yulyakam85@gmail.com>
2022-09-04 14:23:14 +0400

    Add coments

--- Sem2Task15/Program.cs
+++ Sem2Task15/Program.cs
@@ -1,30 +1,30 @@
 ﻿//Программа принимает цифру, обозначающую день нед. и проверяет является ли этот день выход
 Console.WriteLine("Введите номер дня недели, где 1 - это Понедельник ");
 string? D = Console.ReadLine();
-if (D != null)
-{
-    int Day = int.Parse(D);
-    string DayOfWeek = "";
-   switch (Day)
-   {
-     case 1: DayOfWeek = "Понедельник"; break;
-     case 2: DayOfWeek = "Вторник"; break;
-     case 3: DayOfWeek = "Среда"; break;
-     case 4: DayOfWeek = "Четверг"; break;
-     case 5: DayOfWeek = "Пятница"; break;
-     case 6: DayOfWeek = "Суббота"; break;
-     case 7: DayOfWeek = "Воскресенье"; break;
-    
-    default: DayOfWeek = "Такого дня не существует"; break;
-   }
-   if (DayOfWeek == "Такого дня не существует" )
-   {
-     Console.WriteLine("Такого дня не существует! Введите число от 1 до 7");
-   }
-   else
-   {
-    Console.WriteLine((Day == 6 || Day  == 7) ? DayOfWeek + " - выходной день" :
-    DayOfWeek + " - невыходной день");
-   }
-
-}
+  if (D != null)
+  {
+      int Day = int.Parse(D);
+      string DayOfWeek = "";//Объявляем глобальную переменную
+    switch (Day)
+    {// Запускаем оператор выбора, к-й проходит все варианты и при сопадении с чис Day
+    //Выдает рез-т, если совпад нет то default выводит сооб "Такого дня не существует"
+      case 1: DayOfWeek = "Понедельник"; break;
+      case 2: DayOfWeek = "Вторник"; break;
+      case 3: DayOfWeek = "Среда"; break;
+      case 4: DayOfWeek = "Четверг"; break;
+      case 5: DayOfWeek = "Пятница"; break;
+      case 6: DayOfWeek = "Суббота"; break;
+      case 7: DayOfWeek = "Воскресенье"; break;
+      
+      default: DayOfWeek = "Такого дня не существует"; break;
+    }
+    if (DayOfWeek == "Такого дня не существует" ) //Для особо одаренных конкретизируем ввод
+    { //Для особо одаренных конкретизируем ввод
+      Console.WriteLine("Такого дня не существует! Введите число от 1 до 7");
+    }
+    else
+    {//Проверяем если введено число 6 или 7 выводим выходной день
+      Console.WriteLine((Day == 6 || Day  == 7) ? DayOfWeek + " - выходной день" :
+      DayOfWeek + " - невыходной день");
+    }
+  }
