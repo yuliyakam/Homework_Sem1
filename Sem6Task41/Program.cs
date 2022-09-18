a68a6f375ccf1f385131a9a38ef95da398003c97
Yuliya <yulyakam85@gmail.com>
2022-09-18 21:35:50 +0400

    Add Solution Sem6Task41, Sem6Task43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿//Сколько чисел больше 0 с клавиатуры ввел пользователь
+//Считываем данные, для ввода отриц.чисел используем тип данных double
+double ReadData(string line)
+{   
+    Console.Write(line);   
+    //Считываем число и преобразуем из строки в double
+    double x1= double.Parse(Console.ReadLine() ?? "0"); 
+    //Возвращаем значение
+    return x1;  
+}
+//Анализ чисел на положительность и подсчет тех, что >0
+int Count(double num)
+{
+    int res = 0;
+    for (int i = 0; i < num; i++)
+    {
+        double number = ReadData("");
+        if (number>0)
+        {
+            res++;
+        }        
+    }
+    return res;
+}
+//Вывод результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+double countNumber = ReadData("Введите планируемое для анализа на положительность количество чисел ");
+Console.WriteLine("Вводите числа по одному и нажимайте Enter");
+PrintResult("Количество введенных положительных чисел = " + Count(countNumber));
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Нахождения координаты точки пересечения прямы, заданных уравнениями y=k1x+b1 & y=k2x+b2
+//Коэффициенты k1, k2, b1, b2 задаются пользователем
+
+//Т.к коэффициенты в уравнении могут быть отрицательными используем тип double
+double ReadData(string line)
+{   
+    Console.Write(line);   
+    //Считываем число и преобразуем из строки в double
+    double x1= double.Parse(Console.ReadLine() ?? "0"); 
+    //Возвращаем значение
+    return x1;  
+}
+//Расчет координат точки пересечения
+double[] Point(double k1, double b1, double k2, double b2)
+{
+    double[] outArr = new double[2];
+    outArr[0] = (b1 - b2) / (k2 - k1);
+    outArr[1] = outArr[0]*k1+b1;
+    return outArr;
+}
+//Печатаем одномерный массив
+void Print1DArray( string line, double[] array)
+{
+    Console.WriteLine(line);
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write(array[i]+",");
+    }
+    Console.WriteLine(array[array.Length-1]);
+}
+
+double k1 = ReadData("Введите коэффициент k1: ");
+double b1 = ReadData("Введите коэффициент b1: ");
+double k2 = ReadData("Введите коэффициент k2: ");
+double b2 = ReadData("Введите коэффициент b2: ");
+Print1DArray("Координаты точки пересечения: ", Point(k1, b1, k2, b2));
